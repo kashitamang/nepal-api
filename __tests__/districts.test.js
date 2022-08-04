e49143625d5d08ff67b78eb84362a960594777c5
KASHI TAMANG <tamang.kashi@gmail.com>
2022-08-04 14:52:57 -0700

    adding test for #GET /districts

---
+++ __tests__/districts.test.js
@@ -0,0 +1,25 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  
+  it('#GET /districts should return a list of districts', async () => {
+    const res = await request(app).get('/districts');
+    expect(res.body.length).toEqual(3);
+    expect(res.body[0]).toEqual(
+      {
+        id: expect.any(String),
+        name: expect.any(String),
+        population: expect.any(String)
+      });
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
