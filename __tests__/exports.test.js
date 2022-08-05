47eff02e21195199d7eba38e1a8286c79db8f92b
KASHI TAMANG <tamang.kashi@gmail.com>
2022-08-05 11:00:08 -0700

    adding test for get all exports

---
+++ __tests__/exports.test.js
@@ -0,0 +1,25 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('#GET /exports should return a list of all exports', async () => {
+    const res = await request(app).get('/exports');
+    expect(res.body.length).toEqual(3);
+    expect(res.body[0]).toEqual(
+      {
+        id: expect.any(String),
+        type: expect.any(String)
+      });
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+});
