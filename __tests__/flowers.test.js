d4f6f712c4c3cbd26b71c00e070af2a7ebb34bd2
KASHI TAMANG <tamang.kashi@gmail.com>
2022-08-05 09:45:54 -0700

    adding get flowers test

---
+++ __tests__/flowers.test.js
@@ -0,0 +1,20 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('#GET /flowers should return a list of all flower names', async () => {
+    const res = await request(app).get('/flowers');
+    expect(res.body.length).toEqual(3);
+    expect(res.body[0]).toEqual(
+      {
+        id: expect.any(String),
+        name: expect.any(String)
+      });
+  });
+});
